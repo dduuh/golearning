55cbf2658cf9d8962d2e45b90c92595c8f4be179
dduuh <147307716+dduuh@users.noreply.github.com>
2025-02-15 14:40:10 +0300

    Add files via upload

---
+++ LETSGOOOOOO/project/cmd/app/main.go
@@ -0,0 +1,17 @@
+package main
+
+// func Rev(str string) string {
+// 	runes := []rune(str)
+// 	length := len(runes)
+
+// 	for i := 0; i < length/2; i++ {
+// 		j := length - 1 - i
+// 		runes[i], runes[j] = runes[j], runes[i]
+// 	}
+
+// 	return string(runes)
+// }
+
+func main() {
+
+}
---
+++ LETSGOOOOOO/project/pkg/test1/for.go
@@ -0,0 +1,10 @@
+package test1
+
+import "fmt"
+
+func test1(n int) {
+	for i := 0; i < n; i++ {
+		fmt.Println(i)
+	}
+	return
+}
